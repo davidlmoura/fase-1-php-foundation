08bb87a984661e436c6b699730e238bb5a9137fa
David Moura <davidmoura@gmail.com>
2015-12-28 22:38:57 -0200

    Configurando o Bootstrap

--- index.php
+++ index.php
@@ -1,11 +1,72 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Projeto Fase 1 - PHP Foundation</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <body>
-    <h1>Home!</h1>
-    <script src="http://code.jquery.com/jquery.js"></script>
-  </body>
-</html> 
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <title>Projeto Fase 1 - PHP Foundation</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Site Simples">
+  <meta name="author" content="David Moura">
+
+  <link rel="stylesheet" type="text/css" href="css/css.css">
+
+  <?php require_once("bootstrap_files.php"); ?>
+
+</head>
+
+<body>
+
+<div class="container-narrow">
+
+  <div class="masthead">
+    <ul class="nav nav-pills pull-right">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+    <h3 class="muted">Project name</h3>
+  </div>
+
+  <hr>
+
+  <div class="jumbotron">
+    <h1>Super awesome marketing speak!</h1>
+    <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+    <a class="btn btn-large btn-success" href="#">Sign up today</a>
+  </div>
+
+  <hr>
+
+  <div class="row-fluid marketing">
+    <div class="span6">
+      <h4>Subheading</h4>
+      <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+      <h4>Subheading</h4>
+      <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+      <h4>Subheading</h4>
+      <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+    </div>
+
+    <div class="span6">
+      <h4>Subheading</h4>
+      <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+      <h4>Subheading</h4>
+      <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+      <h4>Subheading</h4>
+      <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="footer">
+    <p>&copy; Company 2013</p>
+  </div>
+
+</div>
+
+</body>
+</html>
