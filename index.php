f23e722568c3109e2a0b247e51f72641e4fb5884
David Moura <davidmoura@gmail.com>
2015-12-28 22:17:02 -0200

    Adicionando restantes dos arquivos, incluindo a index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: David
- * Date: 28/12/2015
- * Time: 22:04
- */
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Projeto Fase 1 - PHP Foundation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <h1>Home!</h1>
+    <script src="http://code.jquery.com/jquery.js"></script>
+  </body>
+</html>
