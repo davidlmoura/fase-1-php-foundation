9e84b3d77bd9bfcba046462776d2fc5971c3bcd7
David Moura <davidmoura@gmail.com>
2016-02-07 21:20:28 -0200

    Fixture Corrigido - Adicionado nova coluna com Conteudo

--- fixture.php
+++ fixture.php
@@ -14,18 +14,27 @@ echo "Criando Tabela";
 $conn->query("CREATE TABLE paginas (
       id INT NOT NULL AUTO_INCREMENT,
       pagina VARCHAR(255) CHARACTER SET 'utf8' NULL,
+      conteudo VARCHAR(255) CHARACTER SET 'utf8' NULL,
       PRIMARY KEY (id));");
 echo " - OK\n";
 
 echo "Inserindo Dados";
-for($x = 0; $x <= 9; $x++) {
 
-    $pagina = "Teste {$x}";
+$PaginasPossiveis = ['home','empresa','produtos','servicos','contato'];
 
-    $smt = $conn->prepare("INSERT INTO paginas (pagina) VALUE (:pagina);");
+$i = 0;
+foreach($PaginasPossiveis as $Possiveis) {
+
+    $conteudo = "Teste {$i}";
+    $pagina = $Possiveis[$i];
+
+    $smt = $conn->prepare("INSERT INTO paginas (pagina, conteudo) VALUE (:pagina,:conteudo);");
     $smt->bindParam(":pagina",$pagina);
+    $smt->bindParam(":conteudo",$conteudo);
     $smt->execute();
+
+    $i++;
 }
 echo " - OK\n";
 
-echo "#### Concluído ####\n";
+echo "#### Concluido ####\n";
