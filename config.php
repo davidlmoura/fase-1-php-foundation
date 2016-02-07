23ebd16c9c552c919777b4f247be57aa83ab3656
David Moura <davidmoura@gmail.com>
2016-02-07 19:41:45 -0200

    Arquivo Config criado

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'db' => array(
+        'host' => 'localhost',
+        'dbname' => 'teatrola_foundation',
+        'user' => 'teatrola_found',
+        'password' => 'Foundation1',
+
+    )
+);
